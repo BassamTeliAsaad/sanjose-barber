32d2ee671e06824e9e69a7aa8e787e4ae2befa9a
BassamTeliAsaad <86104012+BassamTeliAsaad@users.noreply.github.com>
2025-11-14 01:19:17 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,8 @@
 from app import app
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=5000)
+@app.before_request
+def create_tables():
+    if not hasattr(app, 'tables_created'):
+        db.create_all()
+        app.tables_created = True
